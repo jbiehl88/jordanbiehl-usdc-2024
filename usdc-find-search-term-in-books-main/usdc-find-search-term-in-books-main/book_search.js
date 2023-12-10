1e5b6fbf79f5c7770110198baf16b5588fad6cc9
Jordan <99503935+jbiehl88@users.noreply.github.com>
2023-12-10 15:39:29 -0600

    Add files via upload

---
+++ usdc-find-search-term-in-books-main/usdc-find-search-term-in-books-main/book_search.js
@@ -0,0 +1,229 @@
+/** 
+ * RECOMMENDATION
+ * 
+ * To test your code, you should open "tester.html" in a web browser.
+ * You can then use the "Developer Tools" to see the JavaScript console.
+ * There, you will see the results unit test execution. You are welcome
+ * to run the code any way you like, but this is similar to how we will
+ * run your code submission.
+ * 
+ * The Developer Tools in Chrome are available under the "..." menu, 
+ * futher hidden under the option "More Tools." In Firefox, they are 
+ * under the hamburger (three horizontal lines), also hidden under "More Tools." 
+ */
+
+/**
+ * Searches for matches in scanned text.
+ * @param {string} searchTerm - The word or term we're searching for. 
+ * @param {JSON} scannedTextObj - A JSON object representing the scanned text.
+ * @returns {JSON} - Search results.
+ * */ 
+ function findSearchTermInBooks(searchTerm, scannedTextObj) {
+    /** You will need to implement your search and 
+     * return the appropriate object here. */
+    const results = [];
+
+    for (const book of scannedTextObj) {
+        for (const content of book.Content) {
+            if (content.Text.includes(searchTerm)) {
+                results.push({
+                    ISBN: book.ISBN,
+                    Page: content.Page,
+                    Line: content.Line,
+                });
+            }
+        }
+    }
+
+    var result = {
+        SearchTerm: searchTerm,
+        Results: results,
+    };
+
+    return result; 
+}
+
+/** Example input object. */
+const twentyLeaguesIn = [
+    {
+        "Title": "Twenty Thousand Leagues Under the Sea",
+        "ISBN": "9780000528531",
+        "Content": [
+            {
+                "Page": 31,
+                "Line": 8,
+                "Text": "now simply went on by her own momentum.  The dark-"
+            },
+            {
+                "Page": 31,
+                "Line": 9,
+                "Text": "ness was then profound; and however good the Canadian\'s"
+            },
+            {
+                "Page": 31,
+                "Line": 10,
+                "Text": "eyes were, I asked myself how he had managed to see, and"
+            } 
+        ] 
+    }
+]
+
+
+const theDog = [
+    {
+        "Title": "The Dog and the Squirrel",
+        "ISBN": "9990000528531",
+        "Content": [
+            {
+                "Page": 12,
+                "Line": 3,
+                "Text": "ran away with the leash dragging behind. Jacob ran"
+            },
+            {
+                "Page": 12,
+                "Line": 4,
+                "Text": " after the dog but was unable to match his speed. The "
+            },
+            {
+                "Page": 12,
+                "Line": 5,
+                "Text": "squirrel the dog was chasing, ran up a tree to avoid "
+            } 
+        ] 
+    }
+]
+
+const theCat = [
+    {
+        "Title": "The Cat is Mean",
+        "ISBN": "9110000555031",
+        "Content": [] 
+    }
+]
+    
+/** Example output object */
+const twentyLeaguesOut = {
+    "SearchTerm": "the",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 9
+        }
+    ]
+}
+
+const twentyLeaguesOut2 = {
+    "SearchTerm": "The",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 8
+        }
+    ]
+}
+
+const theDogOut = {
+    "SearchTerm": "dog",
+    "Results": [
+        {
+            "ISBN": "9990000528531",
+            "Page": 12,
+            "Line": 4
+        },
+        {
+            "ISBN": "9990000528531", 
+            "Page": 12,
+            "Line": 5
+        }
+    ]
+}
+
+const theCatOut = {
+    "SearchTerm": "cat",
+    "Results": [
+        {
+            "ISBN": "9110000555031", 
+            "Page": 12,
+            "Line": 5
+        }
+    ]
+}
+
+
+/*
+ _   _ _   _ ___ _____   _____ _____ ____ _____ ____  
+| | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___| 
+| | | |  \| || |  | |     | | |  _| \___ \ | | \___ \ 
+| |_| | |\  || |  | |     | | | |___ ___) || |  ___) |
+ \___/|_| \_|___| |_|     |_| |_____|____/ |_| |____/ 
+                                                      
+ */
+
+/* We have provided two unit tests. They're really just `if` statements that 
+ * output to the console. We've provided two tests as examples, and 
+ * they should pass with a correct implementation of `findSearchTermInBooks`. 
+ * 
+ * Please add your unit tests below.
+ * */
+
+/** We can check that, given a known input, we get a known output. */
+const test1result = findSearchTermInBooks("the", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesOut) === JSON.stringify(test1result)) {
+    console.log("PASS: Test 1");
+} else {
+    console.log("FAIL: Test 1");
+    console.log("Expected:", twentyLeaguesOut);
+    console.log("Received:", test1result);
+}
+
+/** We could choose to check that we get the right number of results. */
+const test2result = findSearchTermInBooks("the", twentyLeaguesIn); 
+if (test2result.Results.length == 1) {
+    console.log("PASS: Test 2");
+} else {
+    console.log("FAIL: Test 2");
+    console.log("Expected:", twentyLeaguesOut.Results.length);
+    console.log("Received:", test2result.Results.length);
+}
+
+const test3result = findSearchTermInBooks("darkness", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesOut) === JSON.stringify(test3result)) {
+    console.log("PASS: Test 3");
+} else {
+    console.log("FAIL: Test 3");
+    console.log("Expected:", {SearchTerm: 'darkness', Results: Array(1)});
+    console.log("Received:", test3result);
+}
+
+const test4result = findSearchTermInBooks("The", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesOut2) === JSON.stringify(test4result)) {
+    console.log("PASS: Test 4");
+} else {
+    console.log("FAIL: Test 4");
+    console.log("Expected:", twentyLeaguesOut2);
+    console.log("Received:", test4result);
+}
+
+const test5result = findSearchTermInBooks("dog", theDog);
+if (JSON.stringify(theDogOut) === JSON.stringify(test5result)) {
+    console.log("PASS: Test 5");
+} else {
+    console.log("FAIL: Test 5");
+    console.log("Expected:", theDogOut);
+    console.log("Received:", test5result);
+}
+
+const test6result = findSearchTermInBooks("cat", theCat);
+if (JSON.stringify(theCatOut) === JSON.stringify(test6result)) {
+    console.log("PASS: Test 6");
+} else {
+    console.log("FAIL: Test 6");
+    console.log("Expected:", theCatOut);
+    console.log("Received:", test6result);
+}
+
+
+
+
